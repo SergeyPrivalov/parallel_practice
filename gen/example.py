da7cf62c0c6d8a9e3c57e1713fa33293e7f2209c
suhoy <suhoy@studyHumster.at.urfu.ru>
2017-11-21 12:20:14 +0500

    new sigma

--- gen/example.py
+++ gen/example.py
@@ -13,11 +13,8 @@ def main():
 	                for y in Y:
 	                    print('{0} {1} {2:.1f}'.format(x, y, 200 * np.e ** (-(np.abs(x) + np.abs(y))/75) + 1000), file=f)
 	                    print('{0} {1} {2:.1f}'.format(x, y, 100 * np.e ** (-(np.abs(x) + np.abs(y))/75) + 1500), file=f2)
-	                    print('{0} {1} {2:.1f}'.format(x, y, 50 if x == 0 and y == 0 else 50.0 / np.sqrt(x*x + y*y) * np.sin(x + y)), file=f3)
+	                    print('{0} {1} {2:.1f}'.format(x, y, 20 if x == 0 and y == 0 else 50.0 / np.sqrt(x*x + y*y) * np.sin((x + y)/10)), file=f3)
 
 
 main()
-
-
-
 

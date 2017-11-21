5acce7090cd89dce011a3910d9689d32cc3885a7
suhoy <suhoy@studyHumster.at.urfu.ru>
2017-11-21 11:36:53 +0500

    new h1 h2

--- gen/example.py
+++ gen/example.py
@@ -9,8 +9,8 @@ def main():
         with open('H2.dat', 'w') as f2:
             for x in X:
                 for y in Y:
-                    print('{0} {1} {2:.1f}'.format(x, y, 200 * np.e ** (-(np.abs(x) + np.abs(y)) ** 0.5) + 1000), file=f)
-                    print('{0} {1} {2:.1f}'.format(x, y, 100 * np.e ** (-(np.abs(x) + np.abs(y)) ** 0.45) + 1500), file=f2)
+                    print('{0} {1} {2:.1f}'.format(x, y, 200 * np.e ** (-(np.abs(x) + np.abs(y))/75) + 1000), file=f)
+                    print('{0} {1} {2:.1f}'.format(x, y, 100 * np.e ** (-(np.abs(x) + np.abs(y))/75) + 1500), file=f2)
 
 
 
